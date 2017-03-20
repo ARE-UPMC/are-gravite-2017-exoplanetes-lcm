0b9e2997c2617e055c8f73f61059037c3de85bc1
Lorris2709 <lorris.carton@orange.fr>
2017-03-20 09:44:35 +0100

    Update Début du code.py

--- Début du code.py
+++ Début du code.py
@@ -2,17 +2,17 @@
 """
 Created on Lundi 06 Mars 2017 - ARE - 
 
-@author: BOUKHALFI MOURAD 3679709
+@author: BOUKHALFI MOURAD 3679709 CARTON LORRIS 3521310
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
-circle1 = plt.Circle((0, 0), 0.3, color="yellow")
+circle1 = plt.Circle((0, 0), 0.3, color="red", label="Centre de masse")
 fig= plt.figure()
 fig.set_dpi(100)
-fig.set_size_inches(10, 10)
+fig.set_size_inches(50, 50)
 
 
 
@@ -20,9 +20,7 @@ ax=plt.axes(xlim=(-4, 4), ylim=(-4, 4))
 ax.add_artist(circle1)
 #Tracer des cercles#
 theta = np.linspace(0, 2*np.pi, 60)
-#Orbite1#
-x = 2* np.cos(theta)
-y = 2* np.sin(theta)
+
 #Orbite2#
 x1 =  3* np.cos(theta)
 y1 = 3* np.sin(theta)
@@ -30,21 +28,21 @@ y1 = 3* np.sin(theta)
 x2 = np.cos(theta)
 y2 = np.sin(theta)
 
-#Affichage de toute les orbites#
-plt.plot(x, y,x1,y1,x2,y2,color="black")
+#Affichage des orbites#
+plt.plot(x1,y1,x2,y2,color="black")
 
-size = 0.2
+size = 0.1
 xcenter = 0
 ycenter = 0
 radius = 3
 
 
 
-size2 = 0.1
-radius2=2
+size2 = 0.3
+radius2=1
 
-patch = plt.Circle((5,-5), size, fc='b')
-patch2 = plt.Circle((3, -3), size2, fc='r')
+patch = plt.Circle((5,-5), size, fc='b', label="Exoplanete")
+patch2 = plt.Circle((0, -2), size2, fc='yellow', label="Soleil")
 
 def init():
     patch.center = (xcenter, ycenter)
@@ -55,18 +53,22 @@ def init():
 
 def animate(t):
     x,y = patch.center
-    x = xcenter + radius * np.sin(np.radians(t))
-    y = ycenter + radius * np.cos(np.radians(t))
+    x = xcenter + radius * np.cos(np.radians(t))
+    y = ycenter  - radius * np.sin(np.radians(t))
     patch.center = (x,y)
     x,y = patch2.center
-    x = xcenter + radius2 * np.sin(np.radians(t))
-    y = ycenter + radius2 * np.cos(np.radians(t))
+    x = xcenter - radius2 * np.cos(np.radians(t))
+    y = -(ycenter - radius2 * np.sin(np.radians(t)))
     patch2.center= (x,y)
     
     return patch, patch2,
 
 
 
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=300, interval=20, blit=True)
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=1,
+           ncol=2, mode="expand", borderaxespad=0, handles=[circle1, patch, patch2])
+
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=1000, interval=20, blit=True)
+
 
 plt.show()
